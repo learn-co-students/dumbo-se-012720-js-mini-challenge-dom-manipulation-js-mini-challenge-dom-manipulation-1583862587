0529c37812c03ddf15d45d9232b5b19c2041ae41
Fika98 <Afzalov98@gmail.com>
2020-03-10 15:57:34 -0400

    deliverables done

--- index.js
+++ index.js
@@ -4,10 +4,35 @@ console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
+document.querySelector("#header").style.color = "red"
 
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
 
+console.log("PLAYERS array looks like this:", PLAYERS)
+
+const liverpoolPlayers = document.querySelector('.player-container')
+
+function renderPlayers(playerlObj) {
+
+const outerPlayers = document.createElement("li")
+
+outerPlayers.innerHTML = `
+<div class="player" data-number="${playerlObj.number}">
+<h3>
+  ${playerlObj.name} (<em>${playerlObj.nickname}</em>)
+</h3>
+<img src="${playerlObj.photo}" alt="${playerlObj.name}">
+</div>
+`
+liverpoolPlayers.append(outerPlayers)
+}
+PLAYERS.forEach(renderPlayers)
+
+  
 
 /***** Deliverable 4 *****/
+
+const removeRa = document.querySelector("[data-number = '7']")
+
+removeRa.remove()
